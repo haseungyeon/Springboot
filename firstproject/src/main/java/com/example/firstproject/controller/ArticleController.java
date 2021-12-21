ff225f43fed1b748758cd9330de41488f1e51e50
Desktop <gktmdudl@korea.ac.kr>
2021-12-21 23:39:07 +0900

    form with dto added

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,17 +1,20 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
-    @GetMapping("/news")
-            public String articleContents(Model model){
-        String contents = (String)model.getAttribute("contents");
-        String anker = (String)model.getAttribute("name");
-        model.addAttribute("article", contents);
-        model.addAttribute("anker", anker);
-        return "article/news";
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "articles/create";
     }
 }
